60ceff0dbb0a57561a3bd60b5d0b5c99d3dcff1e
rafahg <r.hgar78@gmail.com>
2020-11-05 18:07:26 +0000

    Unit test for 2 characters string passed, green

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+ruby '2.7.1'
+
+gem 'capybara', group: :test
+gem 'pg'
+gem 'rspec', group: :test
+gem 'rubocop', '0.79.0'
+gem 'simplecov', require: false
+gem 'simplecov-console', require: false
+gem 'sinatra'
--- lib/cypher.rb
+++ lib/cypher.rb
@@ -7,9 +7,21 @@ class Cypher
   end
   
   def cypher(string, rotation)
-    rotated_dictionary = dictionary.rotate(rotation) # Rotating the dictionary "r" places, using rotate method.
-    index = dictionary.index(string) # Obtaining the number of the actual index in the original dictionary.
-    return rotated_dictionary[index] # Obtaining the letter in the rotated dictionary using the previous index.
+    encoded_string = string.chars.map {|char| converter(char,rotation) }
+     # Rotating the dictionary "r" places, using rotate method.
+    #index = dictionary.index(string) # Obtaining the number of the actual index in the original dictionary.
+    return encoded_string.join # Obtaining the letter in the rotated dictionary using the previous index.
+  end
+
+
+
+# HELPER METHOD
+
+
+  def converter(char, rotation)
+    rotated_dictionary = dictionary.rotate(rotation)
+    index = dictionary.index(char)
+    return rotated_dictionary[index]
   end
 
 end
